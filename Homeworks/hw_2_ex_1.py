e4258f07a4f6d508f4cb2ca36bba41f29aba364e
Edgar <18edox@gmail.com>
2019-03-22 00:52:58 +0400

    add boolean comment

--- Homeworks/hw_2_ex_1.py
+++ Homeworks/hw_2_ex_1.py
@@ -1,19 +1,17 @@
 while True:
     user_input = input('Please input an integer')
+#    perfect_count = False
     if user_input.isdecimal():
         user_input = int(user_input)
         for digit in range(1, user_input):
             sum_divisible = 0
-            perfect_count = False
             for every_digit in range(1, (digit // 2) + 1):
                 if digit % every_digit == 0:
                     sum_divisible = sum_divisible + every_digit
-            if perfect_count is False and sum_divisible == digit:
+            if sum_divisible == digit:
+#           if perfect_count is True:
                 print(sum_divisible)
-        else:
-            print('No perfect numbers in this range')
-
+#            if perfect_count is False:
+#                print('No perfect numbers in this range')
     else:
         print('Invalid input')
-
-#print('No perfect numbers in this range')
