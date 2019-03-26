3f6ae82e2374c3da874442a417f79cf85696a699
Mushegh Petrosyan <p.mushegh@gmail.com>
2019-03-26 23:03:15 +0400

    most frequent word is in progress

--- Homeworks/hw_3_ex_2.py
+++ Homeworks/hw_3_ex_2.py
@@ -1,11 +1,15 @@
-text_string = str()
-text_dict = dict()
-
 with open('text.txt') as file:
-    data = file.read().replace('\n', '')
-    data = data.replace('.', '')
-    data = data.lower()
-    data = data.split()
-print(data)
+    data = file.read().replace('\n', '').replace('.', '').lower().split()
+    result = {}
+    for word in data:
+        if result.get(word, 0) == 0:
+            result[word] = 1
+        else:
+            result[word] += 1
+    print(result)
 
+    # value = result.values()
+    # keys = result.keys()
 
+    # max_element = max(zip(result.values(), result.keys()))
+    # print(zip(result.values(), result.keys()))
