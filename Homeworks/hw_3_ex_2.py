f25190b84126983036d21268b241d4b5e6062a9d
edgar.ramazyan <18edox@gmail.com>
2019-03-27 13:22:30 +0400

    hw_3_ex_2.py is solved

--- Homeworks/hw_3_ex_2.py
+++ Homeworks/hw_3_ex_2.py
@@ -1,12 +1,31 @@
 with open('text.txt') as file:
     data = file.read().replace('\n', '').replace('.', '').lower().split()
     result = {}
+    new_result = {}
     for word in data:
         if result.get(word, 0) == 0:
             result[word] = 1
         else:
             result[word] += 1
     print(result)
+    key_max1 = max(result.keys(), key=(lambda k: result[k]))
+    # print(type(result[key_max1]))
+    # print(type(key_max1))
+    new_result.update({key_max1: result[key_max1]})
+    del result[key_max1]
+    key_max2 = max(result.keys(), key=(lambda k: result[k]))
+    # print(result[key_max2])
+    # print(key_max2)
+    new_result.update({key_max2: result[key_max2]})
+    del result[key_max2]
+    key_max3 = max(result.keys(), key=(lambda k: result[k]))
+    # print(result[key_max3])
+    # print(key_max3)
+    new_result.update({key_max3: result[key_max3]})
+    # new_result[key_max1] = result[key_max1]
+    # new_result[key_max2] = result[key_max2]
+    # new_result[key_max3] = result[key_max3]
+    print(new_result)
 
     # value = result.values()
     # keys = result.keys()
