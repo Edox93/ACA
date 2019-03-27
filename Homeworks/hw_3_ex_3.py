4da6e59727d99661a5db4d3da25575be54519b2d
edgar.ramazyan <18edox@gmail.com>
2019-03-27 17:14:13 +0400

    added frequent words with collections module

---
+++ Homeworks/hw_3_ex_3.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+with open('text.txt') as file:
+    data = file.read().replace('\n', '').replace('.', '').lower().split()
+    result = {}
+    new_result = {}
+    for word in data:
+        if result.get(word, 0) == 0:
+            result[word] = 1
+        else:
+            result[word] += 1
+Counter = Counter(result)
+first_three_frequent_words = Counter.most_common(3)
+print(first_three_frequent_words)
