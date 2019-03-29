ab7f0bc99960ca1168a4862d1688de9e808a0436
Edgar <18edox@gmail.com>
2019-03-30 01:53:26 +0400

    temperature ex was finished

--- Homeworks/hw_3_ex_6.py
+++ Homeworks/hw_3_ex_6.py
@@ -1,53 +1,64 @@
 def f_cls_to_kvn(celsius):
     kelvin = celsius + 273.15
-    return kelvin
+    return round(kelvin, 2)
 
 
 def f_cls_to_fht(celsius):
     fahrenheit = celsius * (9 / 5) + 32
-    return fahrenheit
+    return round(fahrenheit, 2)
 
 
 def f_kvn_to_cls(kelvin):
     celsius = kelvin - 273.15
-    return celsius
+    return round(celsius, 2)
 
 
 def f_kvn_to_fht(kelvin):
     fahrenheit = kelvin * (9 / 5) - 459.67
-    return fahrenheit
+    return round(fahrenheit, 2)
 
 
 def f_fht_to_cls(fahrenheit):
     celsius = (fahrenheit - 32) * (5 / 9)
-    return celsius
+    return round(celsius, 2)
 
 
 def f_fht_to_kvn(fahrenheit):
     kelvin = (fahrenheit + 459.67) * (5 / 9)
-    return kelvin
+    return round(kelvin, 2)
 
 
-user_input = input('Please input a temperature: ')
-temp_args = ('F', 'K', 'C')
-to_convert = input('Convert to. for (C) please input C for (F) -> F, and for (K) -> K: ')
-user_input_list = []
-str_from_user_input_list = ''
+print('Supported temperature formats: 36.6 C | 97.88 F | 309.75 K')
+user_input = input('Temperature: ').upper()
+valid_temperature_formats = ['F', 'K', 'C']
+user_input_list = user_input.split()
+convert_to = input('Convert to (C(elsius) | K(elvin) | F(ahrenheit)): ').upper()
 
-user_input = user_input.replace(' ', '').upper()
-if user_input[-1] in temp_args and user_input[0].isdigit() and user_input[-2].isdigit():
-    for str_el in user_input:
-        user_input_list.append(str_el)
+if len(user_input_list) == 2 and (not user_input_list[0].isalpha()) and user_input_list[1].isalpha() \
+        and (convert_to in valid_temperature_formats) and (user_input_list[1] in valid_temperature_formats):
+
+    temperature = float(user_input_list[0])
+    temperature_format = user_input_list[1]
+
+    if temperature_format != convert_to:
+        if temperature_format == 'C':
+            if convert_to == 'K':
+                print(user_input, '=', f_cls_to_kvn(temperature), 'K')
+            if convert_to == 'F':
+                print(user_input, '=', f_cls_to_fht(temperature), 'F')
+
+        if temperature_format == 'K':
+            if convert_to == 'C':
+                print(user_input, '=', f_kvn_to_cls(temperature), 'C')
+            if convert_to == 'F':
+                print(user_input, '=', f_kvn_to_fht(temperature), 'F')
+
+        if temperature_format == 'F':
+            if convert_to == 'K':
+                print(user_input, '=', f_fht_to_kvn(temperature), 'K')
+            if convert_to == 'C':
+                print(user_input, '=', f_fht_to_cls(temperature), 'C')
+    else:
+        print(user_input)
 else:
-    print('invalid temperature input')
-user_input_list.insert(-1, '')
-
-#for i in range(len(user_input_list) - 2):
-str_from_user_input_list = ''.join(str(e) for e in user_input_list)
-str_from_user_input_list = str_from_user_input_list[:-1] + str_from_user_input_list[5:]
-print(str_from_user_input_list)
-if str_from_user_input_list.isdigit():
-    print(type(int(str_from_user_input_list)))
-elif str_from_user_input_list.isnumeric():
-    print(type(float(str_from_user_input_list)))
-
+    print('invalid number in list')
