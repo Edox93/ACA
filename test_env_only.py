9a1a798f3c23c823384b8c59b918d95c36970639
Edgar <18edox@gmail.com>
2019-04-08 00:59:07 +0400

    started encode decoding

--- test_env_only.py
+++ test_env_only.py
@@ -1,14 +1,8 @@
-while True:
-    a = input('input:')
-    if not a.isalpha() and not a.isdecimal() and '.' in a and '.' != a[0] or '.' != a[-1]:
-        a = float(a)
-        print(type(a))
-    elif a.isalpha():
-        a = str(a)
-        print(type(a))
-    elif a.isdigit():
-        a = int(a)
-        print(type(a))
-    else:
-        print(type(a),  'i do not know')
+def string_encoder(input_string):
+    bin_string = ' '.join(format(ord(x), 'b') for x in input_string)
+    return bin_string
 
+
+print(string_encoder('acuna matata'))
+
+print(string_encoder('acuna matata')'a string'.decode('ascii')
