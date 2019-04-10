151280c029ecf977f171dd4991a02364cad686bf
Edgar <18edox@gmail.com>
2019-04-11 01:04:41 +0400

    braille in progress

--- Homeworks/hw_4_ex_1/hw_4_ex_1.py
+++ Homeworks/hw_4_ex_1/hw_4_ex_1.py
@@ -1,9 +1,39 @@
-import argparse
+def text_to_braille(text_input):
+    result = ""
+    for char in text_input:
+        value = letter_to_braille(char)
+        for v in value:
+            charBraille = ''
+            for f in v:
+                if f == 1:
+                    charBraille += 'o'
+                else:
+                    charBraille += '-'
+            print(charBraille)
+
+    return result
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('text_input.txt')
-args = parser.parse_args()
-with open(args.filename) as file:
-    text_my = file.read()
-    print(text_my)
+def letter_to_braille(text_input):
+    return braille.get(text_input)
+
+
+def print_braille():
+    print(text_to_braille("ab"))
+
+
+braille = {'a': [[1, 0], [0, 0], [0, 0]],
+           'b': [[1, 0], [1, 0], [0, 0]],
+           'c': [[], [], []],
+           'd': [[], [], []],
+           'e': [[], [], []],
+           'f': [[], [], []],
+           'g': [[], [], []],
+           'h': [[], [], []],
+           'i': [[], [], []],
+           'j': [[], [], []],
+           'k': [[], [], []],
+           'l': [[], [], []]
+           }
+
+print_braille()
